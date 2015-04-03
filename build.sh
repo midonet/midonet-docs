dc14fb691ae5da01956e2d700a88b14c03ffa231
Jan Hilberath <jan@midokura.com>
2015-04-03 13:10:11 +0900

    Preparation for i18n support
    * Append language code to DocBook's book xml:id
    * Append language code to PDF output files (pdfFilenameBase)
    * Append language code to webhelp directory name (webhelpDirname)
    * Configure language code for webhelp indexer (webhelpIndexerLanguage)
    * Rename "title_{lang}.adoc" to "book_title_{lang}.adoc"
    * Rename "abstract_{lang}.xml" to "docinfo_abstract_{lang}.xml"
    * Rename "productname_{lang}.xml" to "docinfo_productname_{lang}.xml"
    * Define and prepend "{sourcedir}" variable to AsciiDoc includes
    * Set language attribute for all documents
    * Add draft warning replacement for all documents
    * Append language code to build directories
    
    Ref: MND-21
    
    Change-Id: Ie76ee20ab0e079510270b8ded1f292ecf66db52b
    Signed-off-by: Jan Hilberath <jan@midokura.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+#
+# Remove stale build target directories manually
+# to avoid trouble when running within Jenkins.
+#
+cd $(dirname "${BASH_SOURCE[0]}") || exit
+find build -type d -name "target" | xargs rm -rfv
+
+#
+# Clean and build.
+#
 mvn clean install 2>&1 | tee build.log
 
 #
@@ -15,9 +27,9 @@ if [ $? -eq 0 ]; then
 fi
 
 #
-# Search the generated HTML for duplicate Disqus identifiers.
+# Search generated HTML files for duplicate Disqus identifiers.
 #
-DUPLICATE_DISQUS_IDS=$(find . -path "*/docbkx/webhelp/*.html" -print0  | xargs -0 grep --no-filename "var disqus_identifier =" | sort | uniq -cd | sort -nr)
+DUPLICATE_DISQUS_IDS=$(find . -path "*/docbkx/webhelp/*.html" -print0 | xargs -0 grep --no-filename "var disqus_identifier =" | sort | uniq -cd | sort -nr)
 if [ "${#DUPLICATE_DISQUS_IDS}" -ne 0 ]; then
     echo ""
     echo "############################################################"
@@ -29,9 +41,9 @@ if [ "${#DUPLICATE_DISQUS_IDS}" -ne 0 ]; then
 fi
 
 #
-# Search the FO for unintended ":leveloffset:" identifiers.
+# Search generated FO files for unintended ":leveloffset:" identifiers.
 #
-UNINTENDED_LEVELOFFSETS=$(find . -path "*/docbkx/autopdf/*.fo" -print0  | xargs -0 grep -l ":leveloffset:" | sort | uniq)
+UNINTENDED_LEVELOFFSETS=$(find . -path "*/docbkx/autopdf/*.fo" -print0 | xargs -0 grep -l ":leveloffset:" | sort | uniq)
 if [[ ! -z ${UNINTENDED_LEVELOFFSETS} ]]; then
     echo ""
     echo "############################################################"
