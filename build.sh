615b219f2ca735d9b28f42f3d4e6f6fc894f3a95
Jan Hilberath <jan@midokura.com>
2015-04-07 15:55:46 +0900

    Enhanced build script.
    Enable "pipefail" and check Maven build command return code.
    
    Change-Id: Iba70cebe49aac44f7ea79d18594ba83e8da6eba4
    Signed-off-by: Jan Hilberath <jan@midokura.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 #
+# Fail on error.
+#
+set -o pipefail
+fail_on_error() {
+    if [ $? -ne 0 ]; then
+        echo "---------------------------------------------------------------"
+        echo "BUILD FAILURE"
+        echo "---------------------------------------------------------------"
+        exit 1
+    fi
+}
+
+#
 # Remove stale build target directories manually
 # to avoid trouble when running within Jenkins.
 #
@@ -11,10 +24,12 @@ find build -type d -name "target" | xargs rm -rfv
 # Clean and build.
 #
 mvn clean install 2>&1 | tee build.log
+fail_on_error
 
 #
 # Search the build log for AsciiDoc warnings.
 #
+echo "Checking for Asciidoctor warnings..."
 ASCIIDOC_WARNINGS=$(grep -i "asciidoctor.*warning" build.log)
 if [ $? -eq 0 ]; then
     echo ""
@@ -25,10 +40,12 @@ if [ $? -eq 0 ]; then
     echo "BUILD FAILURE: ASCIIDOC WARNING(S) FOUND!"
     exit 1
 fi
+echo "Ok."
 
 #
 # Search generated HTML files for duplicate Disqus identifiers.
 #
+echo "Checking for duplicate Disqus IDs..."
 DUPLICATE_DISQUS_IDS=$(find . -path "*/docbkx/webhelp/*.html" -print0 | xargs -0 grep --no-filename "var disqus_identifier =" | sort | uniq -cd | sort -nr)
 if [ "${#DUPLICATE_DISQUS_IDS}" -ne 0 ]; then
     echo ""
@@ -39,10 +56,12 @@ if [ "${#DUPLICATE_DISQUS_IDS}" -ne 0 ]; then
     echo "BUILD FAILURE: DUPLICATE DISQUS ID(S) FOUND!"
     exit 1
 fi
+echo "Ok."
 
 #
 # Search generated FO files for unintended ":leveloffset:" identifiers.
 #
+echo "Checking for wrong level offsets..."
 UNINTENDED_LEVELOFFSETS=$(find . -path "*/docbkx/autopdf/*.fo" -print0 | xargs -0 grep -l ":leveloffset:" | sort | uniq)
 if [[ ! -z ${UNINTENDED_LEVELOFFSETS} ]]; then
     echo ""
@@ -53,3 +72,8 @@ if [[ ! -z ${UNINTENDED_LEVELOFFSETS} ]]; then
     echo "BUILD FAILURE: UNINTENDED \":leveloffset:\" IN FO OUTPUT FOUND!"
     exit 1
 fi
+echo "Ok."
+
+echo "---------------------------------------------------------------"
+echo "BUILD SUCCESS"
+echo "---------------------------------------------------------------"
