0e58a65ac2188b3a38424e087a56e783e537836a
Jan Hilberath <jan@midokura.com>
2015-10-15 17:26:54 +0900

    Update WWW archive
    * Created subdirectories "en" and "ja" for language specific documents
    * Moved existing English language documents to "en" subdirectories
    * Removed symlinks for obsolete version 2015.03
    * Renamed "operation-guide" to "operations-guide"
    * Removed superseded SNAPSHOT versions
    
    * Added new English document:
       * Quick Start Guide: RHEL 7 / Kilo (OSP) [v2015.06-SNAPSHOT]
    
    * Added new Japanese documents:
       * Operations Guide [v2015.06-SNAPSHOT]
       * Quick Start Guide: RHEL 7 / Juno (OSP) [v2015.06-SNAPSHOT]
       * Quick Start Guide: RHEL 7 / Juno (RDO) [v2015.06-SNAPSHOT]
       * Quick Start Guide: RHEL 7 / Kilo (RDO) [v2015.06-SNAPSHOT]
       * Quick Start Guide: Ubuntu 14.04 / Juno [v2015.06-SNAPSHOT]
       * Quick Start Guide: Ubuntu 14.04 / Kilo [v2015.06-SNAPSHOT]
       * Reference Architecture [v2015.06-SNAPSHOT]
       * Troubleshooting Guide [v2015.06-SNAPSHOT]
    
    Change-Id: Ia94e00802caeb49300279064de2a75fcd43ef593
    Signed-off-by: Jan Hilberath <jan@midokura.com>

---
+++ www/docs/ja/reference-architecture/2015.06-SNAPSHOT/content/search/htmlFileList.js
@@ -0,0 +1,25 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "midolman.html";
+fl["1"]= "state_management.html";
+fl["2"]= "midonet_network_architecture.html";
+fl["3"]= "installation_requirements.html";
+fl["4"]= "internal_network.html";
+fl["5"]= "metadata_server.html";
+fl["6"]= "gateway_nodes.html";
+fl["7"]= "zookeeper.html";
+fl["8"]= "solution_components.html";
+fl["9"]= "provider_router.html";
+fl["10"]= "preface.html";
+fl["11"]= "recommended_hardware.html";
+fl["12"]= "bgp_setup_and_layer_3_topologies.html";
+fl["13"]= "compute_host_agents.html";
+fl["14"]= "midonet_cli.html";
+fl["15"]= "bridges.html";
+fl["16"]= "openstack_integration.html";
+fl["17"]= "underlay_network.html";
+fl["18"]= "midoNet_overview.html";
+fl["19"]= "virtual_routers.html";
+fl["20"]= "midonet_key_features.html";
+fl["21"]= "cassandra.html";
+fl["22"]= "midonet_api.html";
