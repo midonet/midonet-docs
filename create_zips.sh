5a61eb379dd25bef9bd2853356f284b130b5bb70
Jan Hilberath <jan@midokura.com>
2016-03-29 11:34:02 +0900

    QSG for SLES 12 / Liberty (SOC)
    Change-Id: I3926dd5384da6028b35c44fd4cd5719c606d12f0
    Ref: MND-174
    Signed-off-by: Jan Hilberath <jan@midokura.com>

--- create_zips.sh
+++ create_zips.sh
@@ -13,6 +13,7 @@ BUILD_DIRS=(
     'quick-start-guide_rhel-7_kilo-rdo_ja'
     'quick-start-guide_rhel-7_liberty-rdo_en'
     'quick-start-guide_rhel-7_liberty-rdo_ja'
+    'quick-start-guide_sles-12_liberty-soc_en'
     'quick-start-guide_ubuntu-1404_kilo_en'
     'quick-start-guide_ubuntu-1404_kilo_ja'
     'quick-start-guide_ubuntu-1404_liberty_en'
