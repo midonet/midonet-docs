4518830dca29a4c2c3f682d7417dc525ff21c628
Jan Hilberath <jan@midokura.com>
2016-05-12 11:40:56 +0900

    WWW archive update
    MidoNet v5.1:
    
    * Operations Guide [EN, v5.1-rev2]
    * Quick Start Guide: RHEL 7 / Kilo (OSP) [EN, v5.1-rev3]
    * Quick Start Guide: RHEL 7 / Kilo (RDO) [EN, v5.1-rev3]
    * Quick Start Guide: RHEL 7 / Liberty (RDO) [EN, v5.1-rev3]
    * Quick Start Guide: SLES 12 / Liberty (SOC) [EN, v5.1-SNAPSHOT (2016-05-12 02:14 UTC)]
    * Quick Start Guide: Ubuntu 14.04 / Kilo [EN, v5.1-rev3]
    * Quick Start Guide: Ubuntu 14.04 / Liberty [EN, v5.1-rev3]
    * REST API [EN, v5.1-rev1]
    
    MidoNet v5.0:
    
    * Quick Start Guide: RHEL 7 / Kilo (OSP) [EN, v5.0-rev4]
    * Quick Start Guide: RHEL 7 / Kilo (RDO) [EN, v5.0-rev4]
    * Quick Start Guide: RHEL 7 / Liberty (RDO) [EN, v5.0-rev4]
    * Quick Start Guide: SLES 12 / Liberty (SOC) [EN, v5.0-SNAPSHOT (2016-05-12 01:44 UTC)]
    * Quick Start Guide: Ubuntu 14.04 / Kilo [EN, v5.0-rev4]
    * Quick Start Guide: Ubuntu 14.04 / Liberty [EN, v5.0-rev4]
    * REST API [EN, v5.0-rev1]
    
    Change-Id: I7fdf929b9b9b664071e2d71a382e200009549f66
    Signed-off-by: Jan Hilberath <jan@midokura.com>

--- www/docs/en/quick-start-guide_sles-12_liberty-soc/5.1-SNAPSHOT/content/search/index-2.js
+++ www/docs/en/quick-start-guide_sles-12_liberty-soc/5.1-SNAPSHOT/content/search/index-2.js
@@ -1,4 +1,6 @@
 //Auto generated index for searching.
+w["edit"]="1,2,4,9,20,22,26";
+w["either"]="0,15";
 w["enabl"]="0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29";
 w["enable_firewal"]="7";
 w["enable_lb"]="11";
